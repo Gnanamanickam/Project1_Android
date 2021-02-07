a1a06cd66c3d0dfa6ffc0c220b0a2ab464fb0e46
Gnanamanickam A <gnanamanickam1995@gmail.com>
2021-02-07 13:56:02 -0600

    Design change

--- app/src/main/java/com/gnani/project1/MainActivity.java
+++ app/src/main/java/com/gnani/project1/MainActivity.java
@@ -16,33 +16,36 @@ public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private Button button2;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.button1);
         Intent intent = new Intent(this, NameCheckActivity.class);
-        button.setOnClickListener(v -> startActivityForResult(intent,1));
+        button.setOnClickListener(v -> startActivityForResult(intent, 1));
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        System.out.println(requestCode + " " + resultCode + " " + data);
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==1) {
+        if (requestCode == 1) {
             button2 = findViewById(R.id.button2);
             Log.i("OnActivityResult", "success");
             if (resultCode == Activity.RESULT_OK) {
-                assert data != null;
-                String name = data.getStringExtra("name");
-                button2.setOnClickListener(view -> Toast.makeText(MainActivity.this, "Incorrect name entered", Toast.LENGTH_LONG).show());
+//                assert data != null;
+                name = data.getStringExtra("name");
+                System.out.println("name in activity" + name);
+//                button2.setOnClickListener(view -> Toast.makeText(MainActivity.this, "Incorrect name entered", Toast.LENGTH_LONG).show());
                 button2.setOnClickListener(view -> {
-                        String ContactName = data.getStringExtra("returnName");
-                        Intent openContacts = new Intent(ContactsContract.Intents.Insert.ACTION).setType(ContactsContract.RawContacts.CONTENT_TYPE);
-                        openContacts.putExtra(ContactsContract.Intents.Insert.NAME, ContactName);
-                        startActivity(openContacts);
+                    Intent openContacts = new Intent(ContactsContract.Intents.Insert.ACTION).setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                    openContacts.putExtra(ContactsContract.Intents.Insert.NAME, name);
+                    startActivity(openContacts);
+                    name = " ";
 
                 });
             } else if (resultCode == Activity.RESULT_CANCELED && data == null) {
@@ -50,5 +53,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        }
     }
+}
--- app/src/main/java/com/gnani/project1/NameCheckActivity.java
+++ app/src/main/java/com/gnani/project1/NameCheckActivity.java
@@ -20,18 +20,20 @@ public class NameCheckActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_check);
         name = (EditText) findViewById(R.id.enterName);
-        String enteredName = name.getText().toString();
-        Intent returnName = new Intent(this, MainActivity.class);
-        returnName.putExtra("name",name.getText().toString());
+
+        Intent intent = new Intent(this, MainActivity.class);
         name.setOnClickListener(v -> {
-            if(enteredName.isEmpty()) {
-                setResult(Activity.RESULT_CANCELED, returnName);
-            }
-            if(enteredName.split(" ").length > 1 && validator(enteredName)) {
-                    setResult(Activity.RESULT_OK, returnName);
-                }
-            else {
-                setResult(Activity.RESULT_CANCELED, returnName);
+            String enteredName = name.getText().toString();
+            intent.putExtra("name", enteredName);
+            if (enteredName.isEmpty()) {
+                setResult(RESULT_CANCELED, intent);
+                System.out.println("if part");
+            } else if (enteredName.split(" ").length > 1 && validator(enteredName)) {
+                setResult(RESULT_OK, intent);
+                System.out.println("else if part");
+            } else {
+                setResult(RESULT_CANCELED, intent);
+                System.out.println("else part");
             }
             finish();
         });
@@ -39,10 +41,11 @@ public class NameCheckActivity extends AppCompatActivity {
 
     public static boolean validator(String name) {
         boolean value = true;
+        System.out.println(name);
         String split_name[] = name.split(" ");
-        for(int i=0;i < name.length(); i++) {
+        for (int i = 0; i < split_name.length; i++) {
             value = split_name[i].matches("([a-zA-z]{0,23})");
-            if(value == false)
+            if (value == false)
                 return value;
         }
         return value;
